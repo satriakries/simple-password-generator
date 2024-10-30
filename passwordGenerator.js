f85cb345e70b1e249a90e9345064952b4855c585
Satria Kries <satriakriess@gmail.com>
2024-10-30 13:48:13 +0700

    change password generation function to meet checkboxes condition

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,5 +1,15 @@
-export function generatePassword(length) {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()_+-";
+export function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const symbols = "~!@#$%^&*()_+-<>?/";
+
+    let charset = "";
+    if (includeLowercase) charset += lowercase;
+    if (includeUppercase) charset += uppercase;
+    if (includeNumbers) charset += numbers;
+    if (includeSymbols) charset += symbols;
+    
     let password = "";
     for (let i=0; i<length; i++) {
         const randomIndex = Math.floor(Math.random() * charset.length);
